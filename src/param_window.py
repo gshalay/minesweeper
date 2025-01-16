a9bc690f76c5d813fb4e118090d130ee99645fea
gshalay <greg_shalay@outlook.com>
2025-01-15 22:43:11 -0500

    WIP

--- src/param_window.py
+++ src/param_window.py
@@ -8,16 +8,14 @@ class ParamWindow(Toplevel):
     def __init__(self, parent, width, height):
         super().__init__(parent)
         self.rows = self.cols = self.mines = None
-        self.geometry(f"{width + 10}x{height + 10}")
+        self.geometry(f"{MIN_WINDOW_WIDTH // 2}x{MIN_WINDOW_HEIGHT // 3}")
         pyglet.options['win32_gdi_font'] = True
         pyglet.font.add_file(FONT_PATH)
         self.technology_bold_font = self.initialize_font(TECH_PATH, FONT_SIZE_LARGE)
         self.update_idletasks()
-        self.grid_propagate(False)
+
         
-        self.grid_rowconfigure(0, weight=1)
-        self.grid_columnconfigure(0, weight=1)
-        
+
         # Params Frame
         self.params_frame = Frame(self, background="red")
         self.params_frame.grid(row=0, column=0, padx=5, pady=5, sticky="nsew")
@@ -26,52 +24,52 @@ class ParamWindow(Toplevel):
         self.params_frame.rowconfigure(2, weight=1)
         self.params_frame.rowconfigure(3, weight=1)
         self.params_frame.columnconfigure(0, weight=1)
-        self.params_frame.columnconfigure(1, weight=4)
-        # self.params_frame.grid_propagate(True)
+        self.params_frame.columnconfigure(1, weight=2)
         self.update_idletasks()
         
-        self.rows_lbl = Label(self.params_frame, font=("Helvetica", 14), anchor="nw")
+        self.rows_lbl = Label(self.params_frame, font=("Helvetica", 14))
         self.rows_lbl.grid(row=0, column=0, padx=2, pady=2, sticky="nsew")
         self.rows_lbl["text"] = "Rows:"
         
-        self.rows_txt = Text(self.params_frame, font=("Helvetica", 14))
+        self.rows_txt = Text(self.params_frame, font=("Helvetica", 14), height=1, width=10)
         self.rows_txt.grid(row=0, column=1, padx=2, pady=2, sticky="nsew")
         self.update_idletasks()
         
-        self.cols_lbl = Label(self.params_frame, font=("Helvetica", 14), anchor="nw")
+        self.cols_lbl = Label(self.params_frame, font=("Helvetica", 14))
         self.cols_lbl.grid(row=1, column=0, padx=2, pady=2, sticky="nsew")
         self.cols_lbl["text"] = "Columns:"
         
-        self.cols_txt = Text(self.params_frame, font=("Helvetica", 14))
+        self.cols_txt = Text(self.params_frame, font=("Helvetica", 14), height=1, width=10)
         self.cols_txt.grid(row=1, column=1, padx=2, pady=2, sticky="nsew")
         self.update_idletasks()
         
-        self.mines_lbl = Label(self.params_frame, font=("Helvetica", 14), anchor="nw")
+        self.mines_lbl = Label(self.params_frame, font=("Helvetica", 14))
         self.mines_lbl.grid(row=2, column=0, padx=2, pady=2, sticky="nsew")
         self.mines_lbl["text"] = "Mines:"
         
-        self.mines_txt = Text(self.params_frame, font=("Helvetica", 14))
+        self.mines_txt = Text(self.params_frame, font=("Helvetica", 14), height=1, width=10)
         self.mines_txt.grid(row=2, column=1, padx=2, pady=2, sticky="nsew")
         self.update_idletasks()
         
-        self.ok_btn = Button(self.params_frame, font=self.technology_bold_font, anchor="nw", command=self.submit)
+        self.ok_btn = Button(self.params_frame, font=self.technology_bold_font, command=self.submit)
         self.ok_btn.grid(row=3, column=0, padx=2, pady=2, sticky="nsew")
         self.ok_btn["text"] = "OK"
         
-        self.cancel_btn = Button(self.params_frame, font=self.technology_bold_font, anchor="nw", command=self.cancel)
+        self.cancel_btn = Button(self.params_frame, font=self.technology_bold_font, command=self.cancel)
         self.cancel_btn.grid(row=3, column=1, padx=2, pady=2, sticky="nsew")
         self.cancel_btn["text"] = "Cancel"
         self.update_idletasks()
-        
+        self.params_frame.grid_propagate(True)
+
         self.protocol("WM_DELETE_WINDOW", self.cancel)
         
     def cancel(self):
         self.destroy()
         
     def submit(self):
-        self.rows = self.rows_txt.winfo_text()
-        self.cols = self.cols_txt.winfo_text()
-        self.mines = self.mines_txt.winfo_text()
+        self.rows = int(self.rows_txt.get(1.0, "end-1c"))
+        self.cols = int(self.cols_txt.get(1.0, "end-1c"))
+        self.mines = int(self.mines_txt.get(1.0, "end-1c"))
         self.destroy()  # Close the child window
 
     def initialize_font(self, path, font_size):
